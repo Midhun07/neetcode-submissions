c8edf7f84c4133f42d411e5c9c6cdcedb67fdca4
Midhun <midhun0407@gmail.com>
2026-07-26 21:33:41 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_str = ""
+        for item in strs:
+            encoded_str = encoded_str + '<bb#>' + item
+        return encoded_str
+    def decode(self, s: str) -> List[str]:
+        s_list = s.strip().split('<bb#>')
+        return s_list[1:]
