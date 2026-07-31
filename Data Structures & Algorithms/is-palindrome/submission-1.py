9f72fe2077b3d7bc1c000faa59f1541dfdf5fc85
Midhun <midhun0407@gmail.com>
2026-07-31 09:41:30 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # Remove all non-alphanumeric character
+        # Start with 2 pointers, one from the start and other from end
+        # Go for N // 2 iterations and check if both pointer data match
+        # if no then return False, if no mismatch till N//2 then return true
+
+        sanitized = ''.join(c for c in s if c.isalnum()).lower()
+        len_s = len(sanitized) // 2
+
+        for i in range(len_s):
+            if sanitized[i] != sanitized[-(i+1)]:
+                return False
+        
+        return True
